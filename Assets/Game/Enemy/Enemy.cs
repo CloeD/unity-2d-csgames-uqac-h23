1a58f0beb3048509d453d67a823ddfb3dc25e27e
Cloé Dutil <Dutil.Cloe@gmail.com>
2023-03-18 10:53:24 -0400

    First version of lvl 1

--- Assets/Game/Enemy/Enemy.cs
+++ Assets/Game/Enemy/Enemy.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int _maxPosRight = 5;
+    private int _maxPosLeft = -5;
+    private bool _goingRight = true;
+    private float _updateMovement = 1.0f;
+    private float _elapsedTime;
+    
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _elapsedTime += Time.deltaTime;
+        if (_elapsedTime >= 1f)
+        {
+            _elapsedTime %= 1f;
+            Vector3 position = transform.position;
+            Vector3 newPosition = new Vector3();
+
+            if (position.x >= _maxPosRight)
+            {
+                _goingRight = false;
+            }
+            else if (position.x <= _maxPosLeft)
+            {
+                _goingRight = true;
+            }
+
+            if (_goingRight)
+            {
+                newPosition.x += 1;
+            }
+            else
+            {
+                newPosition.x -= 1;
+            }
+
+            transform.position = position + newPosition;
+        }
     }
 }
