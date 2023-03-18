1bfdbbe53c74d8217a0a218a1b21a744ce10aae3
Cloé Dutil <Dutil.Cloe@gmail.com>
2023-03-18 15:38:42 -0400

    Final submission

--- Assets/Game/Enemy/Enemy.cs
+++ Assets/Game/Enemy/Enemy.cs
@@ -1,126 +1,130 @@
-using System;
 using UnityEngine;
-using UnityEngine.Events;
+using UnityEngine.Serialization;
 
-public class Enemy : MonoBehaviour
+namespace Game.Enemy
 {
-    // Move target for patrol
-    private TargetJoint2D _targetJoint2D;
-    public Vector2[] PatrolPoints;
-    private int _indexCurrentPatrolPoint;
-    
-    [Header("Move Frequency")]
-    public float moveTimer = 1f;
-    private float startTimer;
-    
-    // Detect player
-    public float detectionDistance = 4f;
-    
-    [field: SerializeField]
-    public Vector3 playerPos;
-    public bool PlayerDetected { get; private set; }
-    public Transform Player { get; private set; }
-
-    [SerializeField]
-    private string detectionTag = "Player";
-    
-    // Search Player
-    private bool SearchPlayer;
-    private int SearchStep;
-    
-    void Start()
+    public class Enemy : MonoBehaviour
     {
-        _targetJoint2D = GetComponent <TargetJoint2D>();
-        NextPatrolPosition();
-    }
+        // Move target for patrol
+        private TargetJoint2D _patrolTarget;
+        [FormerlySerializedAs("PatrolPoints")] public Vector2[] patrolPoints;
+        private int _indexCurrentPatrolPoint;
+    
+        [FormerlySerializedAs("moveTimer")] [Header("Move Frequency")]
+        public float routineTimer = 0.75f;
+        private float _timePassed;
+    
+        // Detect player
+        public float detectionDistance = 4f;
+    
+        [field: SerializeField]
+        public Vector3 playerPos;
 
-    void Update()
-    {
-        if (_targetJoint2D == null)
+        private bool PlayerDetected { get; set; }
+        private Transform Player { get; set; }
+
+        [SerializeField]
+        private string detectionTag = "Player";
+    
+        // Search Player
+        private bool _searchPlayer;
+        private int _searchStep;
+    
+        void Start()
         {
-            Debug.Log("No target found");
-            return;
+            _patrolTarget = GetComponent <TargetJoint2D>();
+            //NextPatrolPosition();
         }
-        // Player detected in collision box
-        if (PlayerDetected)
+
+        void Update()
         {
-            playerPos = Player.position;
-            Vector3 distance = transform.position - playerPos;
-            Debug.Log("distance magnitude " + distance.magnitude);
-            if (distance.magnitude < detectionDistance)
+            if (_patrolTarget == null)
             {
-                _targetJoint2D.target = playerPos;
+                Debug.Log("No target found");
+                return;
             }
-        }
-        
-        // Move each 1s
-        else if (startTimer >= moveTimer)
-        {
-            if (SearchPlayer)
+            // Player detected in collision box
+            if (PlayerDetected)
             {
-                SearchRoutine();
-                SearchStep += 1;
+                playerPos = Player.position;
+                Vector3 distance = transform.position - playerPos;
+                // Debug.Log("distance magnitude " + distance.magnitude);
+                if (distance.magnitude < detectionDistance)
+                {
+                    _patrolTarget.target = playerPos;
+                }
+            }
+        
+            // Routines
+            else if (_timePassed >= routineTimer)
+            {
+                if (_searchPlayer)
+                {
+                    SearchRoutine();
+                    _searchStep += 1;
+                }
+                else
+                {
+                    NextPatrolPosition();
+                }
+                _timePassed = 0;
             }
             else
             {
-                NextPatrolPosition();
+                _timePassed += Time.deltaTime;
             }
-            startTimer = 0;
         }
-        else
-        {
-            startTimer += Time.deltaTime;
-        }
-    }
 
-    private void NextPatrolPosition()
-    {
-        _indexCurrentPatrolPoint = (_indexCurrentPatrolPoint + 1) % PatrolPoints.Length;
-        _targetJoint2D.target = PatrolPoints[_indexCurrentPatrolPoint];
-    }
-
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.CompareTag(detectionTag))
+        private void NextPatrolPosition()
         {
-            PlayerDetected = true;
-            Player = col.gameObject.transform;
+            _indexCurrentPatrolPoint = (_indexCurrentPatrolPoint + 1) % patrolPoints.Length;
+            _patrolTarget.target = patrolPoints[_indexCurrentPatrolPoint];
         }
-    }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag(detectionTag))
+            {
+                Debug.Log("Must Chase Player");
+                PlayerDetected = true;
+                Player = col.gameObject.transform;
+            }
+        }
     
-    private void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.CompareTag(detectionTag))
+        private void OnTriggerExit2D(Collider2D col)
         {
-            PlayerDetected = false;
-            Player = null;
-            SearchPlayer = true;
-            SearchStep = 0;
-            Debug.Log("Starting Search for Player");
+            if (col.CompareTag(detectionTag))
+            {
+                PlayerDetected = false;
+                Player = null;
+                _searchPlayer = true;
+                _searchStep = 0;
+                Debug.Log("Starting Search for Player");
+            }
         }
-    }
 
-    private void SearchRoutine()
-    {
-        Vector2 pos = transform.position;
-        switch (SearchStep)
+        private void SearchRoutine()
         {
-            case 0:
-                pos.x += detectionDistance;
-                break;
-            case 1:
-                pos.y += detectionDistance;
-                break;
-            case 2:
-                pos.x -= detectionDistance;
-                break;
-            case 3:
-                pos.y -= detectionDistance;
-                SearchStep = 0;
-                SearchPlayer = false;
-                Debug.Log("Search Routine finished");
-                break;
+            Vector2 pos = transform.position;
+            switch (_searchStep)
+            {
+                case 0:
+                    pos.x += detectionDistance;
+                    break;
+                case 1:
+                    pos.y += detectionDistance;
+                    break;
+                case 2:
+                    pos.x -= detectionDistance;
+                    break;
+                case 3:
+                    pos.y -= detectionDistance;
+                    _searchStep = 0;
+                    _searchPlayer = false;
+                    Debug.Log("Search Routine finished");
+                    break;
+            }
+            _patrolTarget.target = pos;
         }
-        _targetJoint2D.target = pos;
     }
 }
